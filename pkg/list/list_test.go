f0bb06371daf01de4d93c53739691643bb4bb276
Mikkel Juul <mju@dmi.dk>
2021-11-01 08:41:03 +0100

    fix: add test, increase scope of the test

--- pkg/list/list_test.go
+++ pkg/list/list_test.go
@@ -1,17 +1,40 @@
-package list_test
+package list
 
 import (
 	"testing"
+	)
 
-	"github.com/1995parham/linkedlist/pkg/list"
-)
-
-func TestListWith3Member(t *testing.T) {
-	l := list.New[int]()
+func TestListWith2Members(t *testing.T) {
+	l := New[int]()
 	l.PushFront(10)
 	l.PushFront(20)
 
 	if l.Len() != 2 {
 		t.Errorf("list should have 2 items but have %d items in it", l.Len())
+	}
+	first := l.Head.(Node[int])
+	if getData[int](first) != 20 {
+		t.Errorf("first item should be 20")
+	}
+	if getData[int](first.next()) != 10 {
+		t.Errorf("next item should be 10")
+	}
+}
+
+
+func TestListWith2MembersBack(t *testing.T) {
+	l := New[int]()
+	l.PushBack(10)
+	l.PushBack(20)
+
+	if l.Len() != 2 {
+		t.Errorf("list should have 2 items but have %d items in it", l.Len())
+	}
+	first := l.Head.(Node[int])
+	if getData[int](first) != 10 {
+		t.Errorf("first item should be 10")
+	}
+	if getData[int](first.next()) != 20 {
+		t.Errorf("next item should be 20")
 	}
 }
